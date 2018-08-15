0598d9f33f3c887ee9c68a18118b168068087820
mom0tomo <momo76w@gmail.com>
2018-08-15 15:06:32 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  root to: 'tops#index' 
+  root to: 'tops#index'
+
+  # users/newではなくusers/signupを使う
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :create]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  # resources :sessions, only: [:new, :create, :destroy]
 end
